73b8280f7fa35f98efe1f22196180a7c75be34e6
abdelrhman <majorabdo12@gmail.com>
2021-09-14 00:13:18 +0200

    first

---
+++ cmake-build-debug/src/CMakeFiles/eyeLike.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/abdelrhman/Downloads/eyeLike-master/src/findEyeCenter.cpp" "/home/abdelrhman/Downloads/eyeLike-master/cmake-build-debug/src/CMakeFiles/eyeLike.dir/findEyeCenter.cpp.o"
+  "/home/abdelrhman/Downloads/eyeLike-master/src/findEyeCorner.cpp" "/home/abdelrhman/Downloads/eyeLike-master/cmake-build-debug/src/CMakeFiles/eyeLike.dir/findEyeCorner.cpp.o"
+  "/home/abdelrhman/Downloads/eyeLike-master/src/helpers.cpp" "/home/abdelrhman/Downloads/eyeLike-master/cmake-build-debug/src/CMakeFiles/eyeLike.dir/helpers.cpp.o"
+  "/home/abdelrhman/Downloads/eyeLike-master/src/main.cpp" "/home/abdelrhman/Downloads/eyeLike-master/cmake-build-debug/src/CMakeFiles/eyeLike.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/cmake-build-debug/CMakeFiles/eyeLike.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eyeLike.dir/findEyeCenter.cpp.o"
+  "CMakeFiles/eyeLike.dir/findEyeCorner.cpp.o"
+  "CMakeFiles/eyeLike.dir/helpers.cpp.o"
+  "CMakeFiles/eyeLike.dir/main.cpp.o"
+  "eyeLike"
+  "eyeLike.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/eyeLike.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
